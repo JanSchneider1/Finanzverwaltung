783a4da6f6564e42fd060fdfb4e4464281a6e256
Jan Schneider <jan.schneider@adesso.de>
2018-12-04 14:51:33 +0100

    Accounting from DB can be displayed in table (accounting.php)

--- html/categories.php
+++ html/categories.php
@@ -11,7 +11,6 @@
 include __DIR__ . "/../Php files/ContentService.php";
 
 $service = new ContentService('derflo@mail.de');
-//var_dump($service->categories);
 foreach ($service->categories as $c){
     echo '<p>'.$c->getName().'</p>';
 }
