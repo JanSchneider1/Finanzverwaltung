9809f4f543d8398e35a8befeef78a4e62233b8d8
Cem Caylak <cem.caylak@adesso.de>
2018-12-12 10:23:30 +0100

    Redesigned Registration.php and Login.php

--- sites/Login.php
+++ sites/Login.php
@@ -1,6 +1,13 @@
 <?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: caylak
+ * Date: 16.11.2018
+ * Time: 14:42
+ */
 session_start();
-require_once __dir__.'Repository.php';
+include __dir__."/../ressources/templates.php";
+require_once __dir__.'/../ressources/Repository.php';
 $repository = new Repository();
 $repository->init();
 
@@ -14,26 +21,26 @@ if(isset($_POST["login"])){
 }
 
 ?>
-    <script>
-        // Example starter JavaScript for disabling form submissions if there are invalid fields
-        (function() {
-            'use strict';
-            window.addEventListener('load', function() {
-                // Fetch all the forms we want to apply custom Bootstrap validation styles to
-                var forms = document.getElementsByClassName('needs-validation');
-                // Loop over them and prevent submission
-                var validation = Array.prototype.filter.call(forms, function(form) {
-                    form.addEventListener('submit', function(event) {
-                        if (form.checkValidity() === false) {
-                            event.preventDefault();
-                            event.stopPropagation();
-                        }
-                        form.classList.add('was-validated');
-                    }, false);
-                });
-            }, false);
-        })();
-    </script>
+<script>
+    // Example starter JavaScript for disabling form submissions if there are invalid fields
+    (function() {
+        'use strict';
+        window.addEventListener('load', function() {
+            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+            var forms = document.getElementsByClassName('needs-validation');
+            // Loop over them and prevent submission
+            var validation = Array.prototype.filter.call(forms, function(form) {
+                form.addEventListener('submit', function(event) {
+                    if (form.checkValidity() === false) {
+                        event.preventDefault();
+                        event.stopPropagation();
+                    }
+                    form.classList.add('was-validated');
+                }, false);
+            });
+        }, false);
+    })();
+</script>
 <!DOCTYPE html>
 <html>
 <head>
@@ -42,35 +49,43 @@ if(isset($_POST["login"])){
     <title>Login</title>
 </head>
 
-<body>
+<body style="background-color: #000000;">
 
 <form method="post" class="needs-validation" novalidate>
     <div class="container" style="max-width: 30%;">
-        <div class="border" style="background-color: lightgrey; padding: 5%; border-radius: 5px;">
+        <div class="border" style="background-color: #333333; padding: 5%; border-radius: 5px;">
             <div class="form-group">
-                <label for="email">E-Mail</label>
-                <input type="email" class="form-control" id="email" name="email" placeholder="Email Adresse" required>
-                <div class="invalid-feedback">
-                    Bitte eine E-Mail eingeben.
+                <h1 style="color: #CCCCCC;" class="text-center">Login</h1>
+                <div class="row">
+                    <div class="col">
+                        <label for="email" style="color: #FEFEFE;">E-Mail</label>
+                        <input type="email" class="form-control" id="email" name="email" placeholder="E-Mail" required>
+                        <div class="invalid-feedback">
+                            Bitte eine E-Mail eingeben.
+                        </div>
+                    </div>
                 </div>
-                <label for="password">Passwort</label>
-                <input type="password" class="form-control" id="password" name="password" placeholder="Passwort" required>
-                <div class="invalid-feedback">
-                    Bitte ein Passwort eingeben.
+                <div class="row">
+                    <div class="col">
+                        <label for="password" style="color: #FEFEFE;">Passwort</label>
+                        <input type="password" class="form-control" id="password" name="password" placeholder="Passwort" required>
+                        <div class="invalid-feedback">
+                            Bitte ein Passwort eingeben.
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <br>
+                        <input type="checkbox" name="cookie" value="logged"><span style="color: white;">Eingeloggt bleiben</span>
+                        <button type="submit" class="btn btn-success float-right" name="login">Einloggen</button>
+                    </div>
                 </div>
             </div>
-            <button type="submit" class="btn btn-primary" name="login">Einloggen</button>
         </div>
     </div>
 </form>
 </body>
+<?php printFooter(); ?>
 
 </html>
-
-<?php
-/**
- * Created by IntelliJ IDEA.
- * User: caylak
- * Date: 16.11.2018
- * Time: 14:42
- */
