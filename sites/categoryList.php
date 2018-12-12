2c0c9d4ce30f63c31fc684814702ab04d1105b3e
Florian Albers <Florian.Albers@adesso.de>
2018-12-12 13:51:18 +0100

    started work on categoryList

--- sites/categoryList.php
+++ sites/categoryList.php
@@ -24,19 +24,18 @@
             $service = new ContentService('derflo@mail.de');
         ?>
     </head>
-    <body>
+    <body class="background">
         <!-- Header -->
         <?php printHeader()?>
-        <!-- Background -->
-        <div class="background">
             <!-- Title -->
             <br/>
             <h1 class="title">Kategorien</h1>
             <br/>
 
             <!-- Categories -->
-            <div class="container">
-                <table class="table table-dark table-hover">
+            <div class="container row">
+                <div class="col-lg-6"></div>
+                <table class="table table-dark table-hover col-lg-5">
                     <thead>
                         <tr>
                             <th>Name</th>
@@ -50,7 +49,7 @@
 
                                 echo <<< Category
                                 <tr id="$id">
-                                    <td class="categoryName value">$name</td>
+                                    <td class="value"><input class="input-group-text" style="background: #31343b; color: white;" id="category_$id" type="text" value="$name"></td>
                                     <td style="text-align: end"><button class="btn btn-dark hvr-reveal" onclick="alterCategory($id)"><span class="fas fa-check"></span></button> <button class="btn btn-dark hvr-reveal" onclick="deleteCategory($id)"><span class="fas fa-trash-alt"></span></button></td>
                                 </tr>
 Category;
@@ -59,8 +58,25 @@ Category;
                     </tbody>
                 </table>
             </div>
-            <?php printFooter(); ?>
-        </div>
 
+            <div class="container row">
+                <div class="col-lg-6"></div>
+                <form method="POST">
+                    <table class="table table-dark table-hover col-lg-5">
+                        <thead>
+                        <tr>
+                            <th>Name</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="value"><input class="input-group-text" style="background: #31343b; color: white;" type="text" name="addcategory_categoryName"></td>
+                                <td style="text-align: end"><button type="button" class="btn btn-dark hvr-reveal" onclick="addAccounting(this.form)">Add</button></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </form>
+            </div>
+            <?php printFooter(); ?>
     </body>
 </html>
