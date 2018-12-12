a5fa5ee9416bca047c1483195f35d852a500996e
Cem Caylak <cem.caylak@adesso.de>
2018-12-12 09:17:07 +0100

    Redesigned Registration.php and Login.php

--- php files/Registration.php
+++ php files/Registration.php
@@ -1,8 +1,16 @@
 <?php
 //Durch require wird die gesamte php-Datei nicht mehr geladen (Essentielle Bestandteile wie Funktionen sollten daher mit require eingebunden werden)
-//require_once verhindert mehrfach Inkludierung wodurch ein Fatal-Error enstehen würde . PHP merkt sich, dass die Datei bereits eingebunden wurde.
+//require_once verhindert mehrfach Inkludierung wodurch ein Fatal-Error enstehen würde. PHP merkt sich, dass die Datei bereits eingebunden wurde.
 //include kriegt man nur ein Error und die PHP-Datei wird weiter geladen.
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: caylak
+ * Date: 16.11.2018
+ * Time: 14:44
+ */
 require_once __dir__."/Repository.php";
+require_once __dir__."/../templates/templates.php";
 $repository = new Repository();
 $repository->init();
 if (isset($_POST["register"]))
@@ -78,22 +86,23 @@ if (isset($_POST["register"]))
         </script>
     </head>
 
-    <body>
+    <body style="background-color: #000000;">
 
     <form class="needs-validation" method="post" novalidate>
-        <div class="container" style="max-width: 30%;">
-            <div class="border" style="background-color: lightgrey; padding: 5%; border-radius: 5px;">
+        <div class="container" style="max-width: 40%; ">
+            <div class="border" style="background-color: #333333; padding: 5%; border-radius: 5px;">
                 <div class="form-group">
+                    <h1 style="color: #CCCCCC;" class="text-center">Registrierung</h1>
                     <div class="row">
                         <div class="col">
-                            <label for="firstname">Vorname</label>
+                            <label for="firstname" style="color: #FEFEFE;">Vorname</label>
                             <input type="text" class="form-control" id="firstname" name="firstname" placeholder="Vorname" required>
                             <div class="invalid-feedback">
                                 Bitte einen Vornamen angeben.
                             </div>
                         </div>
                         <div class="col">
-                            <label for="lastname">Name</label>
+                            <label for="lastname" style="color: #FEFEFE;">Name</label>
                             <input type="text" class="form-control" id="lastname" name="lastname" placeholder="Name" required>
                             <div class="invalid-feedback">
                                 Bitte einen Namen angeben.
@@ -102,7 +111,7 @@ if (isset($_POST["register"]))
                     </div>
                 </div>
                 <div class="form-group">
-                    <label for="email">E-Mail</label>
+                    <label for="email" style="color: #FEFEFE;">E-Mail</label>
                     <input type="email" class="form-control" id="email" name="email" placeholder="Email Adresse" required>
                     <div class="invalid-feedback">
                         Bitte eine E-Mail angeben.
@@ -111,28 +120,21 @@ if (isset($_POST["register"]))
                 <div class="form-group">
                     <div class="row">
                         <span class="col">
-                            <label for="password">Passwort</label>
+                            <label for="password" style="color: #FEFEFE;">Passwort</label>
                             <input type="text" class="form-control" id="password" name="password" placeholder="Passwort" onclick="check();" required>
-                            <label for="confirmPassword">Passwort bestätigen</label>
-                            <input type="text" class="form-control" id="confirmPassword" name="confirmPassword" placeholder="Selbes Passwort"  required>
+                            <input type="text" class="form-control" id="confirmPassword" name="confirmPassword" placeholder="Passwort wiederholden"  required>
                             <div class="invalid-feedback">Bitte ein Passwort.</div>
                             <br>
                             <div id="checker"></div>
+                            <span style="color: white;">Bereits registriert? <a href="./Login.php"><i>Einloggen</i></a></span>
+                            <button type="submit" class="btn btn-success float-right" name="register">Registrieren</button>
                     </div>
+
                 </div>
             </div>
-            <button type="submit" class="btn btn-primary" name="register">Registrieren</button>
         </div>
         </div>
     </form>
     </body>
-
+    <?php printFooter(); ?>
     </html>
-<?php
-/**
- * Created by IntelliJ IDEA.
- * User: caylak
- * Date: 16.11.2018
- * Time: 14:44
- */
-?>
