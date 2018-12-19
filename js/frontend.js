8cce7ceb1613f1a5f1d687682fb69ca00244302d
Jan Schneider <jan.schneider@adesso.de>
2018-12-19 07:14:37 +0100

    DESIGN UPDATE: Major CSS changes!! Project uses now LESS, please add 'general.less' to your pages (other ones deleted), view main page for how to include

--- js/frontend.js
+++ js/frontend.js
@@ -4,11 +4,3 @@ $(".dropdown-menu li a").click(function(){
     $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
     $(this).parents(".dropdown").find('.input').val($(this).data('value'));
 });
-
-/*
-$(".dropdown-toggle").focus(function(){
-    alert($(this).parents(".dropdown").find('.input').val());
-    var a = $(this).parents(".dropdown").find('ul li a');
-    foreach(a){
-  }
-});*/
