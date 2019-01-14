65684735b518457ac897ebd52e1283eb00b9beac
Niklas <niklas.firnges@itc-studenten.de>
2019-01-14 09:08:32 +0100

    Completed AnalyticGraph with Timeframe Dropdown

--- sites/analyticGraph.php
+++ sites/analyticGraph.php
@@ -41,52 +41,105 @@ $service = new ContentService($_SESSION["email"]);
 
     <!-- ChartJS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.bundle.min.js"></script>
-</head>
-<body>
-<header>
-<?php printHeader(); ?>
-</header>
-    <div style="color:white">
-        <canvas id='analyticGraphCanvas'></canvas>
-    </div>
-<footer>
-<?php printFooter();
-    $startDate = '2019-01-10';
-    $endDate = '2019-12-12';
-    $unit = 'day';
+
+    <?php
+    $startDate = null;
+    $endDate = null;
+
     //calculate the budget to begin of specified time to display
     $budget = 0;
     $service->reloadAccountings($service->repo->getAccountingsBeforeDate($service->user->getUserID(),$startDate));
     foreach($service->accountings as $accounting)
     {
-            $budget += $accounting->getValue();
+        $budget += $accounting->getValue();
     };
-    $service->reloadAccountings($service->repo->getAccountingsBetweenDates(1,$startDate, $endDate));
-    $dates = array();
-    $data = array();
-    $zaehler = 0;
-    $currentDate = $service->accountings[0]->getDate();
-    $currentDateTotal = 0;
-    foreach($service->accountings as $accounting)
-    {
-        if($currentDate != $accounting->getDate())
-        {
-            $dates[$zaehler] = $currentDate;
-            $budget += $currentDateTotal;
-            $data[$zaehler] = "{x: $currentDate, y: $budget},";
-            $currentDate = $accounting->getDate();
-            $currentDateTotal = 0;
-            ++$zaehler;
-        }
-        $currentDateTotal += $accounting->getValue();
-    }
-    $dates[$zaehler] = $currentDate;
-    $budget += $currentDateTotal;
-    $data[$zaehler] = "{x: $currentDate, y: $budget},";
 
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $startDate = htmlspecialchars($_POST['start_date']);
+        $endDate = htmlspecialchars($_POST['end_date']);
+        if($endDate < $startDate)
+        {
+            $endDate = $startDate;
+        }
+    }
+    ?>
+</head>
+<body>
+<header>
+<?php printHeader(); ?>
+</header>
+<!-- Filter -->
+<div class="container" style="padding-top: 20px">
+    <table class="table table-hover table-dark">
+        <thead>
+        <tr>
+            <th>Zeitraum</th>
+            <th>Von</th>
+            <th>Bis</th>
+        </tr>
+        </thead>
+        <tbody>
+        <td>
+            <!-- Dropdown: Zeitraum -->
+            <div class="dropdown">
+                <button class="btn dropdown-toggle hvr-grow" type="button" value="Alle" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="true">
+                    <?php echo $startDate != null ? "Eigen" : "Alle"; ?>
+                    <span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                    <li id="month"><a class="dropdown-item effect-underline" data-value="Dieser Monat">Diesen Monat</a></li>
+                    <li id="day"><a class="dropdown-item effect-underline" data-value="Heute">Heute</a></li>
+                    <li id="week"><a class="dropdown-item effect-underline" data-value="Diese Woche">Diese Woche</a></li>
+                    <li id="year"><a class="dropdown-item effect-underline" data-value="Dieses Jahr">Dieses Jahr</a></li>
+                    <li id="own"><a class="dropdown-item effect-underline" data-value="Eigen">Eigen</a></li>
+                </ul>
+            </div>
+        </td>
+        <form method="post" action="analyticGraph.php">
+            <td><input class="form-control input" id="date_1" name="start_date" value="<?php if ($startDate != null) {echo $startDate;} ?>" type="date"></td>
+            <td><input class="form-control input" id="date_2" name="end_date" value="<?php if ($endDate != null) {echo $endDate;}?>" type="date"></td>
+            <td style="text-align: end">
+                <button class="btn hvr-underline-from-left" id="btn_filter" type="submit" style="width:100px">Filtern</button>
+            </td>
+        </form>
+        </tbody>
+    </table>
+</div>
+<div class="container table" style="text-align: center; padding-top: 10px;">
+    <span>Budget over Time</span>
+    <canvas id="outChart" style="min-height: 300px;"></canvas>
+</div>
+<footer>
+<?php printFooter();
+var_dump($startDate);
+var_dump($endDate);
+    $service->reloadAccountings($service->repo->getAccountingsBetweenDates(1,$startDate, $endDate));
+    if($service->accountings != null)
+    {
+        $dates = array();
+        $data = array();
+        $zaehler = 0;
+        $currentDate = $service->accountings[0]->getDate();
+        $currentDateTotal = 0;
+        foreach ($service->accountings as $accounting) {
+            if ($currentDate != $accounting->getDate()) {
+                $dates[$zaehler] = $currentDate;
+                $budget += $currentDateTotal;
+                $data[$zaehler] = "{x: $currentDate, y: $budget},";
+                $currentDate = $accounting->getDate();
+                $currentDateTotal = 0;
+                ++$zaehler;
+            }
+            $currentDateTotal += $accounting->getValue();
+        }
+        $dates[$zaehler] = $currentDate;
+        $budget += $currentDateTotal;
+        $data[$zaehler] = "{x: $currentDate, y: $budget},";
+    }
 echo <<< ChartJS
     <script>
-        var ctx = document.getElementById('analyticGraphCanvas').getContext('2d');
+        var ctx = $('#outChart');
         var analyticLineGraph = new Chart(ctx,
         {
             type: 'line',
@@ -107,7 +160,7 @@ ChartJS;
                 datasets:
                 [
                     {
-                        label: 'Budget over time',
+                        
                         fill: false,
                         borderColor: 'rgb(255,255,255)',
                             data:
@@ -127,13 +180,18 @@ ChartJS;
             },
             options:
             {
-               
+               legend:
+               {
+                    display: false
+               }
             }
         }
         );
     </script>
 ChartJS;
 ?>
+    <script src="../js/timespanDropdown.js"></script>
+    <script src="../js/frontend.js"></script>
 </footer>
 </body>
 </html>
